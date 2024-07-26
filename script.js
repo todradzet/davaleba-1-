526d9dd734d90e80f11e4758e0d1942e58e2a66f
todradzet <tamta.todradze.1@iliauni.edu.ge>
2024-07-26 14:42:03 +0400

    davaleba 1

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Name is : Tamta");
+console.log("Surname is : Todradze");
+console.log("Age : 21");
+console.warn("Email : Test@gmail.com");
+console.warn("Password : 123456");
+console.log("Phone : 555-555-555");
